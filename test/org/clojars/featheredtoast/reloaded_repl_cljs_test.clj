3b0cee1ea41a92254d4879cd378a34f8a01a5314
Jeff Wong <awole20@gmail.com>
2016-12-21 12:25:48 -0800

    cljs

--- test/org/clojars/featheredtoast/reloaded_repl_cljs_test.clj
+++
@@ -1,7 +0,0 @@
-(ns org.clojars.featheredtoast.reloaded-repl-cljs-test
-  (:require [clojure.test :refer :all]
-            [org.clojars.featheredtoast.reloaded-repl-cljs :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
