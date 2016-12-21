1b75163e3f08d0d27e0fbbf4d93263f2d00bf3c8
Jeff Wong <awole20@gmail.com>
2016-12-21 13:51:04 -0800

    update for tests

--- project.clj
+++ project.clj
@@ -4,4 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.stuartsierra/component "0.3.1"]])
+                 [org.clojure/clojurescript "1.8.40" :scope "provided"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [lein-doo "0.1.7"]]
+
+  :plugins [[lein-doo "0.1.7"]
+            [lein-cljsbuild "1.1.3"]]
+  :hooks [leiningen.cljsbuild]
+
+  :cljsbuild
+  {:builds 
+   {:test
+    {:source-paths ["src" "test"]
+     :compiler {:main org.clojars.featheredtoast.reloaded-repl-cljs-test
+                :output-to "resources/public/js/compiled/testing.js"
+                :optimizations :none}}}})
--- test/org/clojars/featheredtoast/reloaded_repl_cljs_test.cljs
+++ test/org/clojars/featheredtoast/reloaded_repl_cljs_test.cljs
@@ -1,6 +1,10 @@
 (ns org.clojars.featheredtoast.reloaded-repl-cljs-test
   (:require-macros [cljs.test :refer (is deftest testing)])
-  (:require [cljs.test]))
+  (:require
+   [doo.runner :refer-macros [doo-tests]]
+   [cljs.test]))
 
 (deftest example-passing-test
   (is (= 1 1)))
+
+(doo-tests 'org.clojars.featheredtoast.reloaded-repl-cljs-test)
