a3cf8f9ffcae6b0aa1b7b7848b33b3f169ca23de
Jeff Wong <awole20@gmail.com>
2016-12-21 13:54:14 -0800

    no test dependency

--- project.clj
+++ project.clj
@@ -5,11 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.40" :scope "provided"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [lein-doo "0.1.7"]]
+                 [com.stuartsierra/component "0.3.1"]]
 
-  :plugins [[lein-doo "0.1.7"]
-            [lein-cljsbuild "1.1.3"]]
+  :plugins [[lein-cljsbuild "1.1.3"]]
   :hooks [leiningen.cljsbuild]
 
   :cljsbuild
--- test/org/clojars/featheredtoast/reloaded_repl_cljs_test.cljs
+++ test/org/clojars/featheredtoast/reloaded_repl_cljs_test.cljs
@@ -1,10 +1,6 @@
 (ns org.clojars.featheredtoast.reloaded-repl-cljs-test
   (:require-macros [cljs.test :refer (is deftest testing)])
-  (:require
-   [doo.runner :refer-macros [doo-tests]]
-   [cljs.test]))
+  (:require [cljs.test]))
 
 (deftest example-passing-test
   (is (= 1 1)))
-
-(doo-tests 'org.clojars.featheredtoast.reloaded-repl-cljs-test)
